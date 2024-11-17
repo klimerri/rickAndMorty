aa762c4c7eb3cbde25880eb9c0dafbebebc1ca83
limerri <kovaleva.a2006@yandex.ru>
2024-11-17 21:45:48 +0400

    all almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css'
 import { Header } from './header/Header.jsx'
+import { Card } from './card/Card.jsx'
+import { Paginator } from './paginator/Paginator.jsx'
 
 function App() {
-  return <Header />
+  return <>
+    <Header />
+    <div className='container'>
+      <div className='wrapper'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+    <Paginator />
+  </>
 }
 
 export default App
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,9 +1,27 @@
 import './Card.scss'
 
-export function Card () {
+export function Card (props) {
     return <div className='card'>
-        <div className='card__container'>
-            
+            <img src={props.imgLink} className='card__image'></img>
+
+            <div className='card__user'>
+                <div className='name'>
+                    <h1 className='name__heading'>{props.name}Мяу</h1>
+
+                    <h2 className='name__status status_online'>{props.status}Мяу</h2>
+                </div>
+
+                <div className='location'>
+                    <h3 className='location__heading'>Last known location</h3>
+
+                    <span className='location__last-location'>{props.lastLocation}Мяу</span>
+                </div>
+
+                <div className='first-location'>
+                    <h4 className='first-location__heading'>First seen in</h4>
+
+                    <span className='first-location__first-seen'>{props.firstSeen}Мяу</span>
+                </div>
+            </div>
         </div>
-    </div>
 }
